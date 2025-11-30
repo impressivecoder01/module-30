c9043e0f04f9070ead05e82434535e70f218147a
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-01 00:53:20 +0600

    class two

--- classTwo.js
+++ classTwo.js
@@ -1,1 +1,36 @@
 // access value,nested object, optional chaining, dot notation vs bracket notation
+const student = {
+    nam: 'rafin',
+    marks: 87,
+    1: 50,
+    'home-address' : 'kolabagan',
+    family: {
+        address: 'mirpur-10',
+        members: 10,
+        // mother:{
+        //     nam: 'a-b',
+        //     age: 100,
+        //     'birth-date': 1950
+        // }
+    }
+}
+const studentName = student.nam;
+console.log(studentName)
+
+
+const studentNumber = student[1]
+console.log(studentNumber)
+
+const studentAddress = student['home-address']
+console.log(studentAddress)
+
+for(const key in student){
+    console.log(student[key])
+}
+
+console.log(student.family.address)
+console.log(student.family.mother?.age)
+console.log(student.family.mother?.['birth-date'])
+
+// optional chain
+// console.log(student.family.mother?.age)
