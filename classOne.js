75ea4e0761cb1b691238e4ae6228db831ff434a0
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-01 00:33:12 +0600

    class one done

--- classOne.js
+++ classOne.js
@@ -1,1 +1,12 @@
 // Module introduction and basic es6 recap
+function add(num1=0, num2=0){
+    return num1 + num2;
+}
+console.log(add())
+
+const student = {nam: 'nafis', marks: 100};
+const dynamic = `my mark is ${student.marks}`
+console.log(dynamic)
+const students = [3,3,43,254,6,56,774,4,33,2,]
+const a = [...students]
+console.log(a)
