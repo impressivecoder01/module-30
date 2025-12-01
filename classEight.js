8469520afd99e63afb87c674f37536349ed795ae
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-01 22:53:23 +0600

    class eight done

--- classEight.js
+++ classEight.js
@@ -1,1 +1,11 @@
 // (optional) encapsulation and this keyword
+class Vehicle{
+    constructor(type){
+        this.type = type
+    }
+    getThis(){
+        console.log(this)
+    }
+}
+const car1 = new Vehicle('car')
+car1.getThis()
