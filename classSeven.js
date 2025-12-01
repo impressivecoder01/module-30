8e0895e7cbe50503e3105bdd72b1155b70d9caa1
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-01 22:18:32 +0600

    issue in class  in  class seven

--- classSeven.js
+++ classSeven.js
@@ -1,1 +1,41 @@
 // (optional)inheritance prototypical inheritance
+class Dog{
+    constructor(n, bread,age){
+        this.name = n;
+        this.bread = bread
+        this.age = age
+    }
+    eat(){
+        console.log(`${this.n} is eating`)
+    }
+    bark(){
+        console.log(`${this.bread} is barking`)
+    }
+}
+const dog1 = new Dog('tiger','Deshi')
+console.log(dog1)
+dog1.bark()
+class Animal {
+    constructor(n, age){
+        this.n= n
+        this.age = age
+    }
+}
+class Cat extends Animal {
+    constructor(n,color,ag,ate){
+    //    super(n,age)
+        this.color = color
+        
+        this.ate = ate
+    }
+    eat(){
+        console.log(`${this.n} is eating ${this.ate}`)
+    }
+    
+
+}
+const cat1 = new Cat('baggira', 'black', 5 , 'fish')
+console.log(cat1)
+// cat1.eat()
+cat1.eat()
+// cat1.age()
--- classSix.js
+++ classSix.js
@@ -18,8 +18,12 @@ class Player {
     goal(){
         console.log('score a goal')
     }
+    eating(){
+        console.log('meat')
+    }
 }
 const player1 = new Player('tom', 33)
 console.log(player1)
+player1.eating()
 // player1.goal()
 // console.log(player1)
