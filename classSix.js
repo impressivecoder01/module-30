fbbacb77009a2bacf66fd06d000995f6d5438b81
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-01 21:28:47 +0600

    class unsolved

--- classSix.js
+++ classSix.js
@@ -1,1 +1,25 @@
 // (optional) introduction to class and objects
+const player = {
+
+}
+
+/**
+ * class --> template
+ * properties
+ * method (a special type of function without a function key word)
+ */
+
+class Player {
+    constructor(n,age){
+        this.location = 'bd'
+        this.age = age;
+        console.log('calling', n)
+    }
+    goal(){
+        console.log('score a goal')
+    }
+}
+const player1 = new Player('tom', 33)
+console.log(player1)
+// player1.goal()
+// console.log(player1)
